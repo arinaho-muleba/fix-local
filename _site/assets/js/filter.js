5548912d66c33bd85e2c3b7b2164f7a6ca875080
Lior-Becker <lior.becker@bbd.co.za>
2024-11-28 11:13:01 +0200

    work in progress

--- _site/assets/js/filter.js
+++ _site/assets/js/filter.js
@@ -1,6 +1,8 @@
 let debounceTimer;
+document.addEventListener("DOMContentLoaded", async () => {
+  const useSemanticSearch = false;
 
-document.addEventListener("DOMContentLoaded", () => {
+  const pagefind = await import("/pagefind/pagefind.js");
   const categoryFilter = document.getElementById("categoryFilter");
   const subcategoryFilter = document.getElementById("subcategoryFilter");
   const locationFilter = document.getElementById("locationFilter");
@@ -102,71 +104,88 @@ document.addEventListener("DOMContentLoaded", () => {
   // Handle subcategory, location, and search changes
   subcategoryFilter.addEventListener("change", filterArticles);
   locationFilter.addEventListener("change", filterArticles);
-  searchInput.addEventListener("input",(event)=>{
+  searchInput.addEventListener("input", (event) => {
     clearTimeout(debounceTimer);
-    debounceTimer=setTimeout(()=>{
-      filterArticles()
-
-    },150)
-  } );
+    debounceTimer = setTimeout(() => {
+      filterArticles();
+    }, 500);
+  });
   // Function to filter articles based on selected filters
   async function filterArticles() {
-    const parent = document.querySelector("#articleList");
-    const spinner=document.querySelector("#spinner-wrapper")
-    parent.style.display="none"
-    spinner.style.display="flex"
     const selectedCategory = categoryFilter.value.toLowerCase();
     const selectedSubcategory = subcategoryFilter.value.toLowerCase();
     const selectedLocation = locationFilter.value.toLowerCase();
     const searchText = searchInput.value.toLowerCase();
+    if (useSemanticSearch) {
+      const parent = document.querySelector("#articleList");
+      const spinner = document.querySelector("#spinner-wrapper");
+      parent.style.display = "none";
+      spinner.style.display = "flex";
+      const baseURL = window.env.BASE_URL + "/content";
+      const url = new URL(baseURL);
 
-    const baseURL = window.env.BASE_URL+"/content";
-    const url = new URL(baseURL);
+      url.searchParams.append("limit", -1);
+      if (selectedCategory !== "") {
+        url.searchParams.append("tags", selectedCategory);
+      }
+      if (selectedLocation !== "") {
+        url.searchParams.append(
+          "selectedLocation",
+          selectedLocation.charAt(0).toUpperCase() +
+            selectedLocation.slice(1).toLowerCase()
+        ); //make it sentence case to work with the back end
+      }
+      if (selectedSubcategory !== "") {
+        url.searchParams.append("tags", selectedSubcategory);
+      }
+      if (searchText !== "") {
+        url.searchParams.append("search", searchText);
+      }
 
-    url.searchParams.append("limit", -1);
-    if (selectedCategory !== "") {
-      url.searchParams.append("tags", selectedCategory);
+      try {
+        const results = await fetch(url.toString());
+        const jsonBody = await results.json();
+
+        const orderMap = new Map(
+          jsonBody.map((item, index) => [item.id, index])
+        );
+
+        const sortedArticles = Array.from(articles).sort(
+          (a, b) => orderMap.get(a.id) - orderMap.get(b.id)
+        );
+        articles.forEach((item) => {
+          item.remove();
+        });
+
+        sortedArticles.forEach((node) => {
+          if (orderMap.has(node.id)) {
+            node.style.display = "";
+          } else {
+            node.style.display = "none";
+          }
+          parent.appendChild(node);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+
+      parent.style.display = "grid";
+      spinner.style.display = "none";
+    }else{
+      pagefind.init();
+
+      const search= await pagefind.search(searchText)
+      const results=await Promise.all(
+      search.results.map(async (page)=>{
+        return await page.data()
+      })
+    )
+      console.log(results)
+      const filteredResults=results.filter((item) =>{
+          return(item.url.split("/").length === 4)
+        }).map((item) => item.url.split("/")[2])
+
+        console.log(filteredResults)
     }
-    if (selectedLocation !== "") {
-      url.searchParams.append(
-        "selectedLocation",
-        selectedLocation.charAt(0).toUpperCase() +
-          selectedLocation.slice(1).toLowerCase()
-      ); //make it sentence case to work with the back end
-    }
-    if (selectedSubcategory !== "") {
-      url.searchParams.append("tags", selectedSubcategory);
-    }
-    if (searchText !== "") {
-      url.searchParams.append("search", searchText);
-    }
-
-    try {
-      const results = await fetch(url.toString());
-      const jsonBody = await results.json();
-
-      const orderMap = new Map(jsonBody.map((item, index) => [item.id, index]));
-
-      const sortedArticles = Array.from(articles).sort(
-        (a, b) => orderMap.get(a.id) - orderMap.get(b.id)
-      );
-      articles.forEach((item) => {
-        item.remove();
-      });
-      
-      sortedArticles.forEach((node) => {
-        if(orderMap.has(node.id)){
-          node.style.display = "";
-        }else{
-          node.style.display="none";
-        }
-        parent.appendChild(node);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-
-    parent.style.display="grid"
-    spinner.style.display="none"
   }
 });
